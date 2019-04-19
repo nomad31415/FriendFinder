8a3b2a0f994480f9061f577ca606da82f4a273e5
mcatoeff@gmail.com <mcatoeff@gmail.com>
2019-04-18 20:15:21 -0700

    initial commit

---
+++ app/data/friends.js
@@ -0,0 +1,20 @@
+var friendArray = [
+	{
+	  "name":"Achmet",
+	  "photo":"https://www.sideshowtoy.com/photo_902322_thumb.jpg",
+	  "scores":[
+	      "5",
+	      "1",
+	      "4",
+	      "4",
+	      "5",
+	      "1",
+	      "2",
+	      "5",
+	      "4",
+	      "1"
+	    ]
+	}
+];
+
+module.exports = friendArray;
